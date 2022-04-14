7b5b85631695f94ddbb39353e0871bc317522002
hamza <hamzakinectro@gmail.com>
2022-04-15 00:08:43 +0500

    remove cached repo

--- server.js
+++ server.js
@@ -62,12 +62,12 @@ const server = new ApolloServer({
   ],
 });
 
-// if (process.env.NODE_ENV == "production") {
-app.use(express.static("client/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
-// }
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 await server.start();
 server.applyMiddleware({
